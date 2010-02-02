110b88f472d5cafc714c9c6e7e4f335c9b81fd3a
macbury <macbury@gmail.com>
2010-02-02 18:05:02 +0100

    Dir creating

--- app.rb
+++ app.rb
@@ -10,6 +10,9 @@ set :views, File.dirname(__FILE__) + '/'
 set :public, File.dirname(__FILE__) + '/assets'
 set :sessions, true
 
+FileUtils.mkdir_p(File.join([File.dirname(__FILE__), '/content', '/page']))
+FileUtils.mkdir_p(File.join([File.dirname(__FILE__), '/content', '/static']))
+
 require "lib/auth"
 require "lib/tags"
 require "lib/pages"
